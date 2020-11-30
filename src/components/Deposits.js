c908106e642cdb372838d6f9adf4a43ad791cece
Marius Zienius <marius.zienius@gmail.com>
2020-11-30 21:21:22 +0100

    dart theme

--- src/components/Deposits.js
+++ src/components/Deposits.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Title from './Title';
+import React from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Title from "./Title";
 
 function preventDefault(event) {
   event.preventDefault();
@@ -10,8 +10,8 @@ function preventDefault(event) {
 
 const useStyles = makeStyles({
   depositContext: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
 
 export default function Deposits() {
